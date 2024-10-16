3be828e699e0f7a59766b7486709667aca945dcd
Rajat Jain <80678116+b2rajat@users.noreply.github.com>
2024-10-16 20:19:31 +0000

    hashword

---
+++ GenerateHashWord/hashWord.js
@@ -0,0 +1,26 @@
+/* String.prototype.replaceAt = function(index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+} */
+
+/* String.prototype.replaceAt = function(index, replacement) {
+  return this.substring(0,index) + replacement + this.substring(index+replacement.length);
+} */
+
+  function generateHashWord(input) {
+    var inputArray = input.split(" ")
+    var hashWord = "#"
+    for (var i = 0; i < inputArray.length; i++) {
+      var curWord = inputArray[i]
+      curWord = curWord.replace(curWord[0], curWord[0].toUpperCase())
+  
+      /* inputArray[i]=inputArray[i].replaceAt(0,inputArray[i][0].toUpperCase()); */
+      hashWord = hashWord + curWord;
+    }
+    return hashWord
+  }
+  
+  var input = "hi i am rajat Jain"
+  console.log(generateHashWord(input))
+  
+  // o/p -> '#HiIAmRajatJain'
+  
