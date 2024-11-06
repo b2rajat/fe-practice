1673f7a926d4059fb34b999a51403568a8e2bbb3
Rajat Jain <80678116+b2rajat@users.noreply.github.com>
2024-11-07 02:01:26 +0530

    rev a string

---
+++ reverseString/reverseString.js
@@ -0,0 +1,11 @@
+// rev a string
+
+const rev = (input) => {
+    const output = input.split('').reverse().join('');
+    console.log(output)
+    return output
+  }
+  
+  rev("hello World") // dlroW olleh
+  
+  
