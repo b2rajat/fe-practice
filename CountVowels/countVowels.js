2f903e47acba216d97aafd898e2991569b1e98a7
Rajat Jain <80678116+b2rajat@users.noreply.github.com>
2024-11-18 00:50:06 +0530

    add example

--- CountVowels/countVowels.js
+++ CountVowels/countVowels.js
@@ -1,18 +1,18 @@
 // count vowels
 
 const countVowels = (input) => {
-    const inputArray = input.split("")
-    console.log(inputArray)
-    const vowelList = ["a", "e", "i", "o", "u"]
-    const count = inputArray.reduce((total, currentElemnt) => {
-      if (vowelList.includes(currentElemnt.toLowerCase())) {
-        return total + 1
-      }
-      return total
-    }, 0)
-    console.log(count)
-    return count
-  }
-  
-  countVowels("abcdeEDCBA") // 4
-  
+  const inputArray = input.split("")
+  console.log(inputArray)
+  const vowelList = ["a", "e", "i", "o", "u"]
+  const count = inputArray.reduce((total, currentElemnt) => {
+    if (vowelList.includes(currentElemnt.toLowerCase())) {
+      return total + 1
+    }
+    return total
+  }, 0)
+  console.log(count)
+  return count
+}
+
+countVowels("abcdeEDCBA") // 4
+countVowels("123abcdeEDCBA123") // 4
